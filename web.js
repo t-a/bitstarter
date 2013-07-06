0f823ea03c487d91323aa55ccd004c8b688fe369
t-a <tizianalberi@virgilio.it>
2013-07-06 15:31:21 -0700

    update web.js with html

--- web.js
+++ web.js
@@ -1,9 +1,19 @@
+
 var express = require('express');
+
+
+var fs = require('fs');
+
+var msg = fs.readFile('index.html', 'utf8', function (err, data) {
+    if (err) throw err;
+    return data;
+});
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+
+  response.send(msg);
 });
 
 var port = process.env.PORT || 5000;
